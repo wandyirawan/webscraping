bf3a5b281f4ce59773d3017af4156e75c7f5ed3d
wandyirawan <wandy.irawan17@gmail.com>
2018-12-19 07:19:09 +0700

    fix production

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 // Create default port to serve the app on
 const port = process.env.PORT || 8080;
-app.listen(port);
-// Log a feedback that this is actually running
-console.log("Server started on port " + port);
+app.listen(port, () => {
+  console.log(`Starting at port : ${port}`); // eslint-disable-line no-console
+});
